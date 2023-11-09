80553cfd23f5ca512e7872509ca67e79cd76ec40
EnglishCNTT <ngocanha3k52@gmail.com>
2023-11-10 06:21:02 +0700

    fix update product

--- admin/src/redux/userRedux.js
+++ admin/src/redux/userRedux.js
@@ -3,6 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const userSlice = createSlice({
   name: "user",
   initialState: {
+    userList: [],
     currentUser: null,
     isFetching: false,
     error: false,
@@ -22,8 +23,29 @@ const userSlice = createSlice({
     logout: (state) => {
       state.currentUser = null;
     },
+    //GET ALL
+    getUserStart: (state) => {
+      state.isFetching = true;
+      state.error = false;
+    },
+    getUserSuccess: (state, action) => {
+      state.isFetching = false;
+      state.userList = action.payload;
+    },
+    getUserFailure: (state) => {
+      state.isFetching = false;
+      state.error = true;
+    },
   },
 });
 
-export const { loginStart, loginSuccess, loginFailure } = userSlice.actions;
+export const {
+  loginStart,
+  loginSuccess,
+  loginFailure,
+  getUserStart,
+  getUserSuccess,
+  getUserFailure,
+  logout,
+} = userSlice.actions;
 export default userSlice.reducer;
